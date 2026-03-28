185660c2280053d398e7ed9cf182ad782af968bf
KejDhruv-Pharbit <dhruv_kejriwal@pharbit.com>
2026-03-28 20:08:42 +0530

    Transition

--- frontend/src/Components/MapBackground.jsx
+++ frontend/src/Components/MapBackground.jsx
@@ -1,7 +1,16 @@
 import "../Styles/Components/MapBackground.css"
 import mapUrl from "../assets/World_Map.svg"
+import { useEffect } from "react";
 
 const MapBackground = () => {
+    useEffect(() => {
+  const map = document.querySelector(".map-svg-layer");
+  if (map) {
+    setTimeout(() => {
+      map.classList.add("map-visible");
+    }, 200); // slight delay feels smoother
+  }
+}, []);
   return (
     <div className="map-wrapper">
       {/* 1. The Dotted Map */}
