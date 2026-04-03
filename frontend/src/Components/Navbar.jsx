6b29de6ae17b2d3504be1b56760ac05613e15e4d
KejDhruv-Pharbit <dhruv_kejriwal@pharbit.com>
2026-04-03 19:59:23 +0530

    Navbar Login

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,19 +1,25 @@
-import "../Styles/Components/Navbar.css"
+import { Link } from "react-router-dom";
+import "../Styles/Components/Navbar.css";
 
 const Navbar = () => {
   return (
     <nav className="navbar-container">
       <div className="navbar-logo">Pharbit</div>
+      
       <ul className="navbar-links">
         <li>Software</li>
         <li>Security</li>
         <li>Developers</li>
         <li>Governance</li>
       </ul>
-      <div className="navbar-menu-icon">
-        <span></span>
-        <span></span>
-      </div>
+
+      {/* The Icon itself is now the Login Link */}
+      <Link to="/auth" className="navbar-menu-link" title="Admin Access">
+        <div className="navbar-menu-icon">
+          <span></span>
+          <span></span>
+        </div>
+      </Link>
     </nav>
   );
 };
